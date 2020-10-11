f1deba10c46528a8ae44fde1e6b707b43f04f32d
Andreas Hilfling Ruud <andruud@hotmail.com>
2020-10-11 12:59:33 +0200

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,7 +23,7 @@ namespace API
 
     public Startup(IConfiguration config)
     {
-        _config = config;
+      _config = config;
     }
 
     // This method gets called by the runtime. Use this method to add services to the container.
@@ -35,6 +35,7 @@ namespace API
         options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
       });
       services.AddControllers();
+      services.AddCors();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -54,6 +55,8 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
       app.UseAuthorization();
 
