b920135a1e1589df118b0d4330d331babfc0f5dd
Andreas Hilfling Ruud <andruud@hotmail.com>
2020-10-17 12:00:32 +0200

    End of section 7

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,24 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Middleware;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using API.Data;
-using Microsoft.EntityFrameworkCore;
-using API.Interfaces;
-using API.Services;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using System.Text;
-using API.Extensions;
 
 namespace API
 {
@@ -40,21 +40,23 @@ namespace API
       services.AddCors();
       services.AddIdentityServices(_config);
 
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
+      //services.AddSwaggerGen(c =>
+      //{
+        //c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+      //});
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-      }
+      // if (env.IsDevelopment())
+      // {
+      //  app.UseDeveloperExceptionPage();
+      //  app.UseSwagger();
+      //  app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+      // }
+
+      app.UseMiddleware<ExceptionMiddleware>();
 
       app.UseHttpsRedirection();
 
