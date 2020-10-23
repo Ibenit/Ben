5fba9298e85f08643c94eaf175535cc25803b653
Andreas Hilfling Ruud <andruud@hotmail.com>
2020-10-23 20:21:17 +0200

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -12,29 +13,44 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-  [Authorize]
-  public class UsersController : BaseApiController
-  {
-    private readonly IUserRepository _userRepository;
-    private readonly IMapper _mapper;
-    public UsersController(IUserRepository userRepository, IMapper mapper)
+    [Authorize]
+    public class UsersController : BaseApiController
     {
-      _mapper = mapper;
-      _userRepository = userRepository;
-    }
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-    {
-      var users = await _userRepository.GetMembersAsync();
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
+            var users = await _userRepository.GetMembersAsync();
 
-      return Ok(users);
-    }
+            return Ok(users);
+        }
 
-    [HttpGet("{username}")]
-    public async Task<ActionResult<MemberDto>> GetUser(string username)
-    {
-      return await _userRepository.GetMemberAsync(username);
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
     }
-  }
 }
