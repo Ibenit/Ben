5617d0aab0f47e7a7a2dde0ee7dcdc927bea3816
Andreas Hilfling Ruud <andruud@hotmail.com>
2020-11-01 12:20:52 +0100

    End of section 17

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using API.SignalR;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -9,24 +10,25 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Extensions
 {
-  public static class ApplicationServiceExtensions
-  {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    public static class ApplicationServiceExtensions
     {
-      services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
-      services.AddScoped<ITokenService, TokenService>();
-      services.AddScoped<IPhotoService, PhotoService>();
-      services.AddScoped<ILikesRepository, LikesRepository>();
-      services.AddScoped<IMessageRepository, MessageRepository>();
-      services.AddScoped<LogUserActivity>();
-      services.AddScoped<IUserRepository, UserRepository>();
-      services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
-      services.AddDbContext<DataContext>(options =>
-      {
-        options.UseSqlite(config.GetConnectionString("DefaultConnection"));
-      });
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddSingleton<PresenceTracker>();
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<LogUserActivity>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
 
-      return services;
+            return services;
+        }
     }
-  }
 }
--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -9,40 +10,57 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
 {
-  public static class IdentityServiceExtensions
-  {
-    public static IServiceCollection AddIdentityServices(this IServiceCollection services,
-        IConfiguration config)
+    public static class IdentityServiceExtensions
     {
-      services.AddIdentityCore<AppUser>(opt =>
-      {
-        opt.Password.RequireNonAlphanumeric = false;
-      })
-          .AddRoles<AppRole>()
-          .AddRoleManager<RoleManager<AppRole>>()
-          .AddSignInManager<SignInManager<AppUser>>()
-          .AddRoleValidator<RoleValidator<AppRole>>()
-          .AddEntityFrameworkStores<DataContext>();
-
-      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-          .AddJwtBearer(options =>
-          {
-            options.TokenValidationParameters = new TokenValidationParameters
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, 
+            IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt =>
             {
-              ValidateIssuerSigningKey = true,
-              IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
-              ValidateIssuer = false,
-              ValidateAudience = false,
-            };
-          });
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddSignInManager<SignInManager<AppUser>>()
+                .AddRoleValidator<RoleValidator<AppRole>>()
+                .AddEntityFrameworkStores<DataContext>();
 
-      services.AddAuthorization(opt =>
-      {
-        opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-        opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
-      });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => 
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                    
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
 
-      return services;
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) && 
+                                path.StartsWithSegments("/hubs"))
+                            {
+                                context.Token = accessToken;
+                            }
+
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.AddAuthorization(opt => 
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+            
+            return services;
+        }
     }
-  }
 }
